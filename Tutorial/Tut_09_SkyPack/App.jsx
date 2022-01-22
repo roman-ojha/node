3aa70ff12f82dc35862fcca3d4f03636c222a1e6
Roman Ojha <razzroman98@gmail.com>
2022-01-22 13:15:23 +0545

    skypack cloud package tut

---
+++ Tutorial/Tut_09_SkyPack/App.jsx
@@ -0,0 +1,11 @@
+import React from "https://cdn.skypack.dev/react";
+
+const App = () => {
+  return (
+    <>
+      <h1>Hello</h1>
+    </>
+  );
+};
+
+export default App;
---
+++ Tutorial/Tut_09_SkyPack/Index.jsx
@@ -0,0 +1,15 @@
+/*
+    -> https://www.skypack.dev/
+    -> here we are create a react application with out installing react , react-dom
+*/
+
+import React from "https://cdn.skypack.dev/react";
+import ReactDOM from "https://cdn.skypack.dev/react-dom";
+import App from "./App";
+
+ReactDOM.render(
+  <>
+    <App />
+  </>,
+  document.getElementById("root")
+);
