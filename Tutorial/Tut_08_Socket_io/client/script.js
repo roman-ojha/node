8aafc149bc8d63b86d05c4d4fbf16f69f0c2744d
Roman Ojha <razzroman98@gmail.com>
2022-01-22 19:19:27 +0545

    socket io finished tutorial

--- Tutorial/Tut_08_Socket_io/client/script.js
+++ Tutorial/Tut_08_Socket_io/client/script.js
@@ -1,13 +1,49 @@
 import io from "socket.io-client";
 
-const socket = io("ws://example.com/my-namespace", {
-  reconnectionDelayMax: 10000,
-  auth: {
-    token: "123",
-  },
-  query: {
-    "my-key": "my-value",
-  },
+const socket = io("http://localhost:3000", {
+  // io is the function that we can call to get a individual socket
+  // to connect to the server we have to spacify the apibase url
+  // reconnectionDelayMax: 10000,
+  // auth: {
+  //   token: "123",
+  // },
+  // query: {
+  //   "my-key": "my-value",
+  // },
+});
+
+// to check the connection
+socket.on("connect", () => {
+  // this will listen to any event, we can create our custom event or use event like 'connect'
+  // 'connect' event will run when we connect to our server
+  displayMessage(`you connected with id: ${socket.id}`);
+
+  // socket.emit(
+  //   "custom event",
+  //   10,
+  //   "hi",
+  //   { a: "a" }
+  //   // emit will take any custom event that we want and send to the server and we can pass any information that we want
+
+  //   // now we have to listen to the event 'custom event' that we have created into the server and we can get the information that we have pass from the server
+  // );
+});
+
+// we can listen to the event that server triggered
+socket.on("send-message-client", (message) => {
+  displayMessage(message);
+});
+
+// to connecte to user namespace we will create a user socket
+const usersocket = io("http://localhost:3000/user", {
+  // if we want to have something like admin namespace that require different credential and different authentication it's realy easy to do with namespace because we can actually connect middleware
+  auth: { token: "test" },
+  // here now we want to pass the authentication in this case we want to pass a token
+});
+
+usersocket.on("connect_error", (error) => {
+  // if we have not pass the auth toke information then
+  displayMessage(error);
 });
 
 const joinRoomButton = document.getElementById("room-button");
@@ -21,12 +57,35 @@ form.addEventListener("submit", (e) => {
   const room = roomInput.value;
   if (message === "") return;
   displayMessage(message);
+  socket.emit(
+    "send-message",
+    message,
+    // here we are creating an socket.emit event and sending message to the server
+    room
+    // what if we want to have a private room where we only want to communicate to certain people and we join a room and only get a messages inside a room
+    // by default every single user in socket io has there own room and that's just their id so, the single user is in room with themselves with this id
+    // so if we want to send the another use we can send it to a room that is their id
+    // we are are passing room property
+  );
 
   messageInput.value = "";
 });
 
 joinRoomButton.addEventListener("click", () => {
+  // what if we want to send the message to multiple people and not to every one this is where id of custom room come in
   const room = roomInput.value;
+  socket.emit(
+    "join-room",
+    room,
+    // here we will call the event 'join-room' and we will pass the 'room'
+    // now we will listen this event to the server and if we will send the message after joining the room then the message will send to those user who had join the room
+
+    // one additional feature that we can do this this 'socket.on' and 'socket.emit' that we can pass a callback function and this callback function will get called from the server back down to the client
+    // NOTE: callback is the last thing that we pass to 'emit'
+    (message) => {
+      displayMessage(message);
+    }
+  );
 });
 
 function displayMessage(message) {
@@ -34,3 +93,24 @@ function displayMessage(message) {
   div.textContent = message;
   document.getElementById("message-container").append(div);
 }
+
+// so now how socket io handle disconnecting and reconnecting
+document.addEventListener("keydown", (e) => {
+  if (e.target.matches("input")) return;
+
+  if (e.key === "c") socket.connect();
+  // if we will press 'c' key then we want to connect to socket
+
+  if (e.key === "d") socket.disconnect();
+  // if we will press 'd' then we want to disconnect socket
+});
+
+let count = 0;
+
+setInterval(() => {
+  // inside this interval we want to send the save message over and over again
+  // socket.emit("ping", ++count);
+  // so when we will press d it means that we are offline from the socket and 'emit' the information from the client to server then it will queue the information that we had emit and when we connected by pressing 'c' then all thos queue message will send to the server
+  // it means that by default socket io when you disconnect will store all of the information that we have send and then once we have receonnect it'll send all those information at once if we don't want to use this feature then we can use the 'volatile' flag
+  socket.volatile.emit("ping", ++count);
+}, 1000);
--- Tutorial/Tut_08_Socket_io/server/server.js
+++ Tutorial/Tut_08_Socket_io/server/server.js
@@ -4,17 +4,112 @@
     Socket IO
         => it will allow us to make connection between a client and server but have that connection persist
             -> in case of fetch or ajax:
-                fetch, ajax (request)===> server (response)===> clinet
+                fetch, ajax (request)===> server (response)===> client
                     -> every time we want api we will have to request to the server
             -> but in case of socket io:
                 -> we are going to setting up something called a web socket and these web sockets are going to connect to the server but that connection stays open so if i make 10 request to the server through our websocket i only every make one connection because that connection stays open
                 -> this is very useful when we have to make a lot of request especially if tey're smaller requests
 */
 
-const io = require("socket.io")(3000);
+// in socket.io also have to admin UI:
+// -> npm i @socket.io/admin-ui
+// https://socket.io/docs/v4/admin-ui/
+
+const { instrument } = require("@socket.io/admin-ui");
+const io = require("socket.io")(3000, {
+  cors: {
+    //   to connected to the client and to solve the cors policy error
+    origin: ["http://localhost:8080", "https://admin.socket.io"],
+  },
+});
 // this is the function and pass it in the port of our server
 
 io.on("connection", (socket) => {
-  // this is going to be a function that runs every single time a clinet connects to our server and it's going to give a socket instance for each one of them
+  // when client connect to server it is call this io.on('connection');
+
+  // this is going to be a function that runs every single time a client connects to our server and it's going to give a socket instance for each one of them
   console.log(socket.id);
+  //   'socket.id' is the random id that is assign to each person when that connect to our server
+
+  //   socket.on("custom event", (number, string, object) => {
+  //     console.log(number, string, object);
+  //   });
+
+  socket.on("send-message", (message, room) => {
+    console.log(message);
+
+    // now we want to send message to our client
+    // io.emit("send-message-client", message);
+    // this will send the message to every single socket out there
+    // it send the request to all the client include the client that make the request in the first place
+
+    // now here we are passing the room id as well for to pass for the the message with in that room
+    if (room === "") {
+      // if we don't have a room it means that this message can be able to brodcast to every one
+      socket.broadcast.emit(
+        "send-message-client",
+
+        // if we want send the message except the client that make the request that we will say
+        message
+      );
+    } else {
+      // if we want send message to the spacific room
+      socket.to(room).emit("send-message-client", message);
+      //   so while using 'to()' this is the broadcast it means that this will send the message to all the use except the use that send the message
+    }
+  });
+  socket.on("join-room", (room, cb) => {
+    //   here now we will listen the event to join a room
+    socket.join(room);
+
+    cb(`joined ${room}`);
+  });
+
+  socket.on("ping", (n) => {
+    console.log(n);
+  });
 });
+
+const userIo = io.of(
+  "/user"
+  //   we can see that Namespace in UI '/' & '/admin' , we can create our own namespace
+  // so every thing that we can do on normal io we can do on this '/user' io
+);
+
+userIo.use(
+  // this is how we make up different middleware and every middleware is going to take a scket
+  (socket, next) => {
+    // 'socket' is all the information for the socket we're connecting with and next is just going to be the next middleware
+
+    // now we can access the auth token here
+    if (socket.handshake.auth.token) {
+      socket.username = getUsernameFromToken(socket.handshake.auth.token);
+      next();
+    } else {
+      // if use didn't pass a token
+      next(new Error("Please send token"));
+    }
+  }
+);
+
+function getUsernameFromToken(token) {
+  // here we can do what ever that we want to do like get access to the database and check toke in there and authticate the use of any thing
+  return token;
+}
+
+userIo.on("connection", (socket) => {
+  // so to connected to this user namespace we have to go to our client and we have to make connection to it
+  console.log("connected to user neamespace with usename " + socket.username);
+});
+
+instrument(
+  io,
+  { auth: false }
+  // in instrument we will going to pass the instance of io
+  // auth:false we don't care about to auth to get into dashboard
+  //   https://admin.socket.io/#/
+  //   we have to add url to cors origin list
+
+  //server URL:
+  // http://localhost:3000/admin
+);
