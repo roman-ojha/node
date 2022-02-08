0905d3bb75cc1cc188aca1b9656c226d0c3876af
Roman Ojha <razzroman98@gmail.com>
2022-02-08 14:08:09 +0545

    completing email validator

--- Tutorial/Tut_10_nodemailer/router/router.js
+++ Tutorial/Tut_10_nodemailer/router/router.js
@@ -1,38 +1,112 @@
+import dotenv from "dotenv";
+dotenv.config();
 import express from "express";
 import nodemailer from "nodemailer";
+import jwt from "jsonwebtoken";
 import path from "path";
 const router = express.Router();
 const __dirname = path.resolve();
 
-const adminEmail = "razzroman99@hotmail.com";
+const adminEmail = "razzroman99@gmail.com";
+
+const transporter = nodemailer.createTransport({
+  // here we have to given the admin email and pass to varify
+  service: "Gmail",
+  auth: {
+    user: adminEmail,
+    pass: process.env.ADMIN_EMAIL_PASS,
+  },
+});
+
+// DataBase
+let userData = [];
 
 router.get("/", (req, res) => {
-  //   const transporter = nodemailer.createTransport({
-  //     // here we have to given the admin email and pass to varify
-  //     auth: {
-  //       user: adminEmail,
-  //       pass: "hello",
-  //     },
-  //   });
-
-  //   const message = {
-  //     // here we are create the message that we want to send
-  //     from: adminEmail,
-  //     to: "razzroman99@gmail.com",
-  //     subject: "sending email with node.js",
-  //     text: "wow! this is good",
-  //     html: "<p> good to go </p>",
-  //   };
-
-  //   transporter.sendMail(message, function (err, info) {
-  //     if (err) {
-  //       console.log("errrrrrrr");
-  //       console.log(err);
-  //       return;
-  //     }
-  //     console.log(info.response);
-  //   });
   res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+router.post("/register", async (req, res) => {
+  try {
+    const { email } = req.body;
+    if (email !== "") {
+      const newUser = {
+        email: email,
+        conformed: false,
+      };
+      jwt.sign(
+        { email: email },
+        process.env.EMAIL_SECRET,
+        {
+          expiresIn: "1d",
+        },
+        (err, emailToken) => {
+          // this is async way it means user will get response first and the email will be send so this way will be much faster then other way
+          const url = `http://localhost:${process.env.PORT}/confirmation/${emailToken}`;
+          const message = {
+            // here we are create the message that we want to send
+            from: adminEmail,
+            to: email,
+            subject: "Conform Email",
+            //   text: "wow! this is good",
+            html: `Please click this url to confirm you registration: <a href="${url}">${url}</a>`,
+          };
+          transporter.sendMail(message, function (err, info) {
+            if (err) {
+              console.log(err);
+              return;
+            }
+            console.log(info.response);
+          });
+        }
+      );
+
+      userData.push(newUser);
+    }
+    res.send(`<h1>Please Conform Email</h1><a href="/login">Login</a>`);
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/confirmation/:token", async (req, res) => {
+  //   we will redirect to this url from the email that we had send to user email
+  try {
+    const verifyToken = jwt.verify(req.params.token, process.env.EMAIL_SECRET);
+    //here we will verify the token
+    const userEmail = verifyToken.email;
+    const userIndex = userData.findIndex((user) => user.email === userEmail);
+    // we will find the user and set conformed = true in database
+    userData[userIndex].conformed = true;
+    console.log(userData[userIndex]);
+    if (userData[userIndex].conformed) {
+      res.send(`<h1>Email: ${userEmail}</h1>`);
+    } else {
+      res.send(`<h1>Please Conform Email</h1><a href="/login">Login</a>`);
+    }
+  } catch (e) {
+    console.log(e);
+  }
+});
+
+router.get("/login", (req, res) => {
+  res.sendFile(path.join(__dirname, "./public/login.html"));
+});
+
+router.post("/login", (req, res) => {
+  const { email } = req.body;
+  if (email !== "") {
+    const user = userData.find((user) => user.email === email);
+    if (user) {
+      if (user.conformed) {
+        // if email is varified
+        res.send(`<h1>Email: ${email}</h1>`);
+      } else {
+        res.send(`<h1>Please Conform Email</h1><a href="/login">Login</a>`);
+      }
+    } else {
+      res.redirect(`http://localhost:${process.env.PORT}`);
+    }
+  }
 });
 
 export default router;
--- Tutorial/Tut_10_nodemailer/src/app.js
+++ Tutorial/Tut_10_nodemailer/src/app.js
@@ -1,14 +1,26 @@
 /*
     -> to send the email admin needs to have there own email for that we as documentation it is not recommended to use gmail instaed we will use outlook
+    -> if you want to use gmail then you have to give access to less secure app:
+        -> https://myaccount.google.com/lesssecureapps
+    -> for 10 minute email:
+        -> https://10minutemail.com/
 */
 import dotenv from "dotenv";
 dotenv.config();
 import express from "express";
 import router from "../router/router.js";
+import bodyParser from "body-parser";
 
 const app = express();
 const PORT = process.env.PORT || 8080;
 
+app.use(express.json());
+app.use(bodyParser.json());
+app.use(
+  bodyParser.urlencoded({
+    extended: false,
+  })
+);
 app.use(router);
 
 app.listen(PORT, () => {
